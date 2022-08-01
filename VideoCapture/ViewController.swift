3c145b1c4d80e0cbf1686c79682c81861f550274
王鹏 <1597419892@qq.com>
2022-08-01 12:50:19 +0800

    new file:   VideoCapture.xcodeproj/project.pbxproj 	new file:   VideoCapture.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   VideoCapture.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   VideoCapture/AppDelegate.swift 	new file:   VideoCapture/Assets.xcassets/AccentColor.colorset/Contents.json 	new file:   VideoCapture/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   VideoCapture/Assets.xcassets/Contents.json 	new file:   VideoCapture/Base.lproj/LaunchScreen.storyboard 	new file:   VideoCapture/Base.lproj/Main.storyboard 	new file:   VideoCapture/Info.plist 	new file:   VideoCapture/SceneDelegate.swift 	new file:   VideoCapture/ViewController.swift 	new file:   VideoCaptureTests/VideoCaptureTests.swift 	new file:   VideoCaptureUITests/VideoCaptureUITests.swift 	new file:   VideoCaptureUITests/VideoCaptureUITestsLaunchTests.swift

---
+++ VideoCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoCapture
+//
+//  Created by 王鹏 on 2022/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
